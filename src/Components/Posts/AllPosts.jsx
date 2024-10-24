4fa5ef14c45f7cda3972d09b9214e4717448d9d2
abraham241 <huguesmboundou@gmail.com>
2024-10-24 14:01:38 +0100

    page allpost

--- src/Components/Posts/AllPosts.jsx
+++ src/Components/Posts/AllPosts.jsx
@@ -1,31 +1,29 @@
-import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../config/firebase';
-import Post from './Post';
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import Post from "./Post";
 
 const AllPosts = () => {
-    const [post,setPosts] = useState([]);
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const querySnapshot = await getDocs(collection(db, "posts"));
-            const postsData = querySnapshot.docs.map(doc => {
-              const data = doc.data();
-              return {
-                id: doc.id,
-                ...data,
-                timestamp: data.timestamp ? data.timestamp.toDate() : new Date(),
-                likedBy: data.likedBy,
-                comments: data.comments
-              };
-            });
-            setPosts(postsData.sort((a, b) => b.timestamp - a.timestamp));  // Trier les posts par date décroissante
-          };
-      
-          fetchPosts();
-      }, []);
-    return (
-            post.map((pt,i) => <Post key={i} post={pt}/>)
-    );
-}
+  const [post, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+      const postsData = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          id: doc.id,
+          ...data,
+          timestamp: data.timestamp ? data.timestamp.toDate() : new Date(),
+          likedBy: data.likedBy,
+          comments: data.comments,
+        };
+      });
+      setPosts(postsData.sort((a, b) => b.timestamp - a.timestamp)); // Trier les posts par date décroissante
+    };
+
+    fetchPosts();
+  }, []);
+  return post.map((pt, i) => <Post key={i} post={pt} />);
+};
 
 export default AllPosts;
