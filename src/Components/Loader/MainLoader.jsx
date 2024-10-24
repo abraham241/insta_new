f5bc9b249d2ebd9671ecbf9084643b28d4e313f4
abraham241 <huguesmboundou@gmail.com>
2024-10-24 05:18:36 +0100

    la page loader

--- src/Components/Loader/MainLoader.jsx
+++ src/Components/Loader/MainLoader.jsx
@@ -1,8 +1,25 @@
+import React from "react";
 import Loader from "react-js-loader";
-const MainLoader = ({size = 20,display = true}) => {
-    return (
-            display ? <Loader type="spinner-default" bgColor={"blue"} size={size} /> : null
-    );
-}
+import PropTypes from "prop-types";
+
+const MainLoader = ({
+  size = 50,
+  display = true,
+  bgColor = "blue",
+  type = "spinner-default",
+}) => {
+  return display ? (
+    <div className="flex justify-center items-center h-full">
+      <Loader type={type} bgColor={bgColor} size={size} />
+    </div>
+  ) : null;
+};
+
+MainLoader.propTypes = {
+  size: PropTypes.number,
+  display: PropTypes.bool,
+  bgColor: PropTypes.string,
+  type: PropTypes.string,
+};
 
 export default MainLoader;
