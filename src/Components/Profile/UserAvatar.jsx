53f4e76a90983b65993b3f28e6deacb1bf9afa86
abraham241 <huguesmboundou@gmail.com>
2024-10-26 10:39:29 +0100

    page UserAvatar les changements des if + modif

--- src/Components/Profile/UserAvatar.jsx
+++ src/Components/Profile/UserAvatar.jsx
@@ -4,32 +4,29 @@ import { doc, onSnapshot } from 'firebase/firestore';
 import { FiUser } from 'react-icons/fi'; 
 
 const UserAvatar = ({ userId }) => {
-  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState('/asset/profil.png');
 
   useEffect(() => {
-    if (userId) {
-      const unsub = onSnapshot(doc(db, 'users', userId), (doc) => {
-        const userData = doc.data();
-        console.log(userData);
-        if (userData && userData.avatar) {
-          setAvatarUrl(userData.avatar);
-        } else {
-          
-          setAvatarUrl('/asset/profil.png');
-        }
-      }, error => {
-        console.log("Failed to fetch avatar:", error);
-        
-        setAvatarUrl('/asset/profil.png');
-      });
+    // Fonction pour gérer les mises à jour du snapshot
+    const fetchAvatar = () => 
+      onSnapshot(
+        doc(db, 'users', userId), 
+        (doc) => setAvatarUrl(doc?.data()?.avatar || '/asset/profil.png'),
+        () => setAvatarUrl('/asset/profil.png')
+      );
 
-      return () => unsub();
-    }
+    const unsubscribe = userId ? fetchAvatar() : () => {}; // Exécute fetch si userId existe
+
+    return () => unsubscribe(); // Nettoyage de l'abonnement
   }, [userId]);
 
   return (
     <div className='overflow-hidden w-[50px] h-[50px] rounded-[99px] flex justify-center items-center'>
-      {avatarUrl ? <img className='w-[90%] h-[90%] object-contain' src={avatarUrl} alt='user avatar'/> : <FiUser size="26px"  className='mb-4 shadow bg-blue-500 text-white p-0 rounded-full hover:bg-blue-600 transition duration-300' />}
+      {avatarUrl ? (
+        <img className='w-[90%] h-[90%] object-contain' src={avatarUrl} alt='user avatar' />
+      ) : (
+        <FiUser size="26px" className='mb-4 shadow bg-blue-500 text-white p-0 rounded-full hover:bg-blue-600 transition duration-300' />
+      )}
     </div>
   );
 };
